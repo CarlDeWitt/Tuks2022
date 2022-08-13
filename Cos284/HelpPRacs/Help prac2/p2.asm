65e4bc673fb6fc8bd75d880d72960d2bc38d47e0
CarlDeWitt <u21444928@tuks.co.za>
2022-08-13 10:57:53 +0200

    print ints

--- Cos284/HelpPRacs/Help prac2/p2.asm
+++ Cos284/HelpPRacs/Help prac2/p2.asm
@@ -1,63 +1,127 @@
 segment .data
 
-statement: dw "Your balance is", 0x0a
-statement_length: equ $-statement
-input: dw "", 0x0a
-input_lenght: equ $-input
-nextLine: dw "\n", 0x0a
-nextLine_lenght: equ $-nextLine
-balance: dq 10, 0x0a
-balance_length: equ $-balance
+welcome: dw "Welcome to ASM Integer Bank!", 0x0a
+welcomeLen: equ $-welcome
+statement: dw "Balance: ", 0x0a
+statementLen: equ $-statement
+transtype1: dw "Please select your transaction type:", 0x0a
+transtypeLen1: equ $-transtype1
+transtype2: dw "1. Withdraw R10", 0x0a
+transtypeLen2: equ $-transtype2
+transtype3: dw "2. Deposit R5", 0x0a
+transtypeLen3: equ $-transtype3
+transtype4: dw "3. Exit", 0x0a
+transtypeLen4: equ $-transtype4
+rem: dw "", 0x0a
+divs: dw "10", 0x0a
+balance dq  12
+nextLine dw 0x0a
+num1 dw 0
+num2 dw 0
 
 section .text
 
-output:
-mov eax, 1
-mov edi, 1
-mov edx, nextLine_lenght
-lea rsi, [nextLine]
-;syscall
+NewLine:
+mov rax, 1
+mov rdi, 1
+mov rsi, nextLine
+mov edx, 1
+syscall
+ret
+
+asciiConversion:
+mov rax, [balance]
+add rdx, 0
+mov rcx, 10
+idiv rcx
+add rax, 48
+mov [num1], rax
+add rdx, 48
+mov [num2], rdx
+
+mov rax,1
+mov rdx,1
+mov rsi, num1
+mov rdx, 1
+mov rax,1
+mov rdx,1
+mov rsi, num2
+mov rdx, 1
+syscall
+
+call NewLine
+
+ret
+
+welcomeMSG:
 
 mov eax, 1
 mov edi, 1
-mov edx, statement_length
+mov edx, welcomeLen
+lea rsi, [welcome]
+syscall
+
+mov eax, 1
+mov edi, 1
+mov edx, statementLen
 lea rsi, [statement]
 syscall
 
 mov eax, 1
 mov edi, 1
-mov edx, balance_length
-lea rsi, [balance]
+mov edx, transtypeLen1
+lea rsi, [transtype1]
 syscall
+
+mov eax, 1
+mov edi, 1
+mov edx, transtypeLen2
+lea rsi, [transtype2]
+syscall
+
+
+mov eax, 1
+mov edi, 1
+mov edx, transtypeLen3
+lea rsi, [transtype3]
+syscall
+
+mov eax, 1
+mov edi, 1
+mov edx, transtypeLen4
+lea rsi, [transtype4]
+syscall
+call NewLine
 ret
 
 inputcall:  
 ;clears the registers and set them all to 0
 mov rax,0
 mov rdi,0
-mov rdx, 0
-mov rsi, 0
+mov rdx,0
+mov rsi,0
 syscall
 
-mov rax,0
-mov rdi,0
-mov rdx, input_lenght
-mov rsi, input
-syscall
+;mov rax,0
+;mov rdi,0
+;mov rdx, input_lenght
+;mov rsi, input
+;syscall
 
 ;print input
-mov eax, 1
-mov edi, 1
-mov edx, input_lenght
-lea rsi, [input]
-syscall
+;mov eax, 1
+;mov edi, 1
+;mov edx, input_lenght
+;lea rsi, [input]
+;syscall
 ;ret
 
 
 global _start
 _start:
 
-;call output
+;call welcomeMSG
+call asciiConversion
 ;call inputcall
 
 ;ret
