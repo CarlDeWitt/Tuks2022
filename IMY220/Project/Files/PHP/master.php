93396a54a62ac5902bebed36571b377046d31df8
CarlDeWitt <u21444928@tuks.co.za>
2022-08-30 07:56:21 +0200

    Update master.php

--- IMY220/Project/Files/PHP/master.php
+++ IMY220/Project/Files/PHP/master.php
@@ -1,6 +1,9 @@
 <?php
 /* DataBase Connection */
-$mysqli = mysqli_connect("localhost", "root", "", "eventsurley");
+//localhost
+// $mysqli = mysqli_connect("localhost", "root", "", "eventsurley");
+// MM server 
+$mysqli = mysqli_connect("localhost", "u21444928", "euuviouc", "u21444928");
 
 // e_users query
 $S_e_users = "SELECT * FROM e_users";
@@ -17,4 +20,3 @@ $I_list = "INSERT INTO `list`";
 // list Events
 $S_list_events = "SELECT * FROM `list_events`";
 $I_list_events = "INSERT INTO `list_events`";
-?>
