5479c971308f8a7178f84b27e93d131b49bc8571
CarlDeWitt <u21444928@tuks.co.za>
2022-10-06 21:18:02 +0200

    Update script.js

--- IMY220/Assignments/Assignment 7/script.js
+++ IMY220/Assignments/Assignment 7/script.js
@@ -1,37 +1,15 @@
 class UsernamePasswordInput extends React.Component {
   constructor(props) {
     super(props);
-    this.submit = this.submit.bind(this);
     this.checkUsername = this.checkUsername.bind(this);
     this.checkPassword = this.checkPassword.bind(this);
     this.nameInput = React.createRef();
     this.passwordInput = React.createRef();
   }
 
-  submit(e) {
-    e.preventDefault();
-    // let username = this.nameInput.current.value;
-    let password = this.passwordInput.current.value;
-    // console.log(username, password);
-    /*PASSWORD*/
-    if (password.length < 7) {
-      console.log("Password must be at least 8 characters long");
-    }
-
-    const upperCaseInLetter = /[A-Z]/;
-
-    if (!password.match(upperCaseInLetter)) {
-      console.log("You need an Upper Case letter in Password");
-    }
-
-    const didgitCheck = /[0-9]/gm;
-    if (!password.match(didgitCheck)) {
-      console.log("Password must contain at least one digit");
-    }
-  }
-
   checkUsername(e) {
     let username = this.nameInput.current.value;
+    let casecheck = true;
     /*USERNAME*/
     const specialChar = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     if (specialChar.test(username)) {
@@ -40,30 +18,42 @@ class UsernamePasswordInput extends React.Component {
     const upperCheck = username[0].toUpperCase();
     if (username[0] !== upperCheck) {
       console.log("username must start with a capital letter");
+      casecheck = false;
     }
     if (username.length < 2) {
       console.log("username must be longer than 2 chars");
+      casecheck = false;
+    }
+
+    if (casecheck) {
+      this.props.validateUser();
     }
   }
 
   checkPassword(e) {
     let password = this.passwordInput.current.value;
-    // console.log(username, password);
-    // console.log(password);
+    let casecheck = true;
     /*PASSWORD*/
     if (password.length < 7) {
       console.log("Password must be at least 8 characters long");
+      casecheck = false;
     }
 
     const upperCaseInLetter = /[A-Z]/;
 
     if (!password.match(upperCaseInLetter)) {
       console.log("You need an Upper Case letter in Password");
+      casecheck = false;
     }
 
     const didgitCheck = /[0-9]/gm;
     if (!password.match(didgitCheck)) {
       console.log("Password must contain at least one digit");
+      casecheck = false;
+    }
+
+    if (casecheck) {
+      this.props.validatePass();
     }
   }
 
@@ -82,16 +72,41 @@ class UsernamePasswordInput extends React.Component {
 }
 
 class LoginForm extends React.Component {
-  constructor(e) {
-    super(e);
+  constructor(props) {
+    super(props);
     this.submit = this.submit.bind(this);
+    this.validateUserfunction = this.validateUserfunction.bind(this);
+    this.validatePassfunction = this.validatePassfunction.bind(this);
+    this.state = { state1: false, state2: false };
+  }
+
+  submit(e) {
+    if (this.state.stat1 == true && this.state.stat2 == true) {
+      console.log("submit");
+    } else {
+      console.log("not submit");
+      e.preventDefault();
+    }
+  }
+
+  validateUserfunction() {
+    this.setState({ state1: true });
+  }
+
+  validatePassfunction() {
+    this.setState({ state2: true });
   }
 
   render() {
     return (
       <form>
-        <UsernamePasswordInput />
-        <button type="submit">Submit</button>
+        <UsernamePasswordInput
+          validateUser={this.validateUserfunction}
+          validatePass={this.validatePassfunction}
+        />
+        <button type="submit" value="Add">
+          Submit
+        </button>
       </form>
     );
   }
