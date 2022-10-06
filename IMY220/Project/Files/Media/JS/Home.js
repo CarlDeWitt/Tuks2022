3e7112b5d1ccd107636e4712a952d4dc38e263ab
CarlDeWitt <u21444928@tuks.co.za>
2022-10-06 07:32:19 +0200

    work

---
+++ IMY220/Project/Files/Media/JS/Home.js
@@ -0,0 +1,191 @@
+/*--------create functionallity ----------*/
+const createButton = document.querySelector(".createSub");
+const rows = document.querySelectorAll(".createRows");
+
+let click = true;
+
+createButton.onclick = function () {
+  if (click) {
+    rows.forEach((e) => {
+      e.classList.toggle("Dsnone");
+
+      e.classList.add("animationOpacity");
+    });
+  } else {
+    rows.forEach((e) => {
+      e.classList.remove("animationOpacity2");
+      e.classList.add("animationOpacity2");
+      setTimeout(function () {
+        e.classList.remove("animationOpacity2");
+        e.classList.toggle("Dsnone");
+      }, 800);
+    });
+  }
+  click ? (click = false) : (click = true);
+};
+/*--------END create functionallity ----------*/
+
+// LOGOUT
+Logout = () => {
+  location.href = "./PHP/logout.php";
+};
+
+// description stuff
+let click2 = true;
+
+// function descAnimation(e) {
+//   if (click2) {
+//     e.classList.add("animationOpacity");
+//   } else {
+//     e.classList.remove("animationOpacity");
+//     e.classList.add("animationOpacity2");
+//     setTimeout(function () {
+//       e.classList.remove("animationOpacity2");
+//     }, 800);
+//   }
+//   click2 ? (click2 = false) : (click2 = true);
+// }
+{
+  /* <a href=\"DSE.html?EID=@$rowE2[id]\"> */
+}
+
+/*--------Nav bar ----------*/
+const navButton1 = document.querySelector("#NavBtn1"); //global variable
+const navButton2 = document.querySelector("#NavBtn2"); //Profile
+const navButton3 = document.querySelector("#NavBtn3"); //Logout
+
+navButton2.onclick = function () {
+  navButton2.classList.add("navActive");
+  navButton3.classList.remove("navActive");
+};
+
+navButton3.onclick = function () {
+  navButton3.classList.add("navActive");
+  navButton2.classList.remove("navActive");
+};
+/*--------END Nav bar ----------*/
+
+/*------End Events like and add--------*/
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.querySelectorAll(".Radd");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+/*-------Events like and add--------*/
+let EventID = -1;
+function heart(name, element) {
+  element.classList.toggle("click");
+  setTimeout(function () {
+    element.classList.toggle("click");
+  }, 500);
+  console.log(name);
+}
+
+function add(name, element) {
+  EventID = name;
+  element.classList.toggle("click");
+  setTimeout(function () {
+    element.classList.toggle("click");
+  }, 500);
+  modal.style.display = "block";
+  console.log(name);
+}
+
+function getListName(userID, listID, element) {
+  element.classList.toggle("click");
+  setTimeout(function () {
+    element.classList.toggle("click");
+  }, 500);
+  if (EventID == -1) {
+    alert("Please select an event");
+    return;
+  }
+  console.log("userID" + userID, "ListID" + listID, "EventID" + EventID);
+
+  $.ajax({
+    type: "POST", // Method type GET/POST
+    url: "./PHP/insertIntoList.php", //Ajax Action url
+    data: { UID: userID, LID: listID, EID: EventID }, //Form Data
+    success: function (data, textStatus, jqXHR) {
+      console.log(data);
+    },
+  });
+  location.reload();
+}
+
+//diplay Description
+function red(e) {
+  // var x = element.querySelector(".description");
+  // var y = element.querySelector(".Rdel");
+  // x.classList.toggle("descHide");
+  // y.classList.toggle("delHide");
+  console.log(e);
+  window.location.replace("DSE.php?EID=" + e);
+}
+
+// function deleteEvent(eventid, element, userid) {
+//   console.log(eventid, userid);
+//   $.ajax({
+//     type: "POST", // Method type GET/POST
+//     url: "./PHP/deleteEvent.php", //Ajax Action url
+//     data: { UID: userid, EID: eventid }, //Form Data
+//     success: function (data, textStatus, jqXHR) {
+//       console.log(data);
+//     },
+//   });
+//   location.reload();
+// }
+
+//likeBTN
+//List delete
+function showList(element) {
+  element.querySelector(".fa-trash-can").classList.toggle("delHide");
+  element.querySelector(".fa-angle-down").classList.toggle("delHide");
+}
+
+function deleteList(listid) {
+  // console.log(listid);
+  $.ajax({
+    type: "POST", // Method type GET/POST
+    url: "./PHP/deleteList.php", //Ajax Action url
+    data: { LID: listid }, //Form Data
+    success: function (data, textStatus, jqXHR) {
+      console.log(data);
+    },
+  });
+  location.reload();
+}
+
+const srchBtn = document.querySelector(".srchtxt");
+
+srchBtn.onclick = () => {
+  console.log("search does not work yet");
+};
+
+// goToProfile
+
+GoToProfile = (userid) => {
+  console.log(userid);
+  location.href = "./profilePage.php?UID=" + userid;
+};
--- IMY220/help/ES6/script.js
+++ IMY220/help/ES6/script.js
@@ -1,32 +1,79 @@
-// Arrow functions
+// // Arrow functions
 
-sum = (a,b) => a+b;
-isPositive = (a) => a>=0;
-randomNum = ()=>Math.random();
-document.addEventListener("click.js", ()=> console.log("Clicked"));
-document.querySelector()
+// sum = (a,b) => a+b;
+// isPositive = (a) => a>=0;
+// randomNum = ()=>Math.random();
+// document.addEventListener("click.js", ()=> console.log("Clicked"));
+// document.querySelector()
 
-console.log(sum(1,2));
-console.log(isPositive(4));
-console.log(randomNum());
+// console.log(sum(1,2));
+// console.log(isPositive(4));
+// console.log(randomNum());
 
-// class
-class Person {
-    constructor(name, age) {
-        this._name = name;
-        this._age = age;
-    }
-    sayName = () => console.log(this._name);
+// // class
+// class Person {
+//     constructor(name, age) {
+//         this._name = name;
+//         this._age = age;
+//     }
+//     sayName = () => console.log(this._name);
     
-    sayAge = () => console.log(this._age);
+//     sayAge = () => console.log(this._age);
     
 
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
-}
-let p = new Person("John", 30);
-p.sayName();
+//     get name() {
+//         return this._name;
+//     }
+//     set name(value) {
+//         this._name = value;
+//     }
+// }
+// let p = new Person("John", 30);
+// p.sayName();
+// var btn = document.querySelector('.btn');
+
+// btn.onclick = dosomething;
+    
+
+// function dosomething(){
+//     var dosomething2 = function(){
+//         console.log('aweh');  
+//     }
+//     return dosomething2();
+// }
+
+// $(`#firstTop`).append(`<h1>HI</h1>`);
+
+// $(`body`).find(`a`).val(1).css("fontSize", "32px");
+
+// $(`body`).each(function(){
+//     console.log($(this).text());
+    
+//     if($(this).text() == 1){
+//         console.log('yapsdo'); 
+//     }else{
+//         console.log('no'); 
+//     }
+// })
+
+$(`div`).each(function(){
+    $(this).each(function(){
+        if($(this).text() === "1"){
+            console.log('yaa');            
+        }else{
+            console.log('na');
+            
+        }
+        console.log($(this).text());
+    })
+
+    // console.log($(this).text());
+    
+    
+    // if($(this).text() == 1){
+    //     console.log('yapsdo'); 
+    // }else{
+    //     console.log('no'); 
+    // }
+})
+    
