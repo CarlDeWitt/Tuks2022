91486f240867e0115a99d6c0974e73442740e2bd
CarlDeWitt <u21444928@tuks.co.za>
2022-10-06 10:47:19 +0200

    work

--- IMY220/Project/Files/PHP/getAllattendingEvents.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require './master.php';
-
-    $query = $S_attending_events;
-    $result = mysqli_query($mysqli, $query);
-    echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
-?>
--- IMY220/Project/Files/PHP/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['UserIndex']);
-    session_destroy();
-    header('Location: ../Index.html');
-?>
